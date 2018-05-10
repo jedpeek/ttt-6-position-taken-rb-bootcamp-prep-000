54bfc2707dcb4133c80fad81c0a4e8343d0a4b3f
Jed Peek <jedpeek@yahoo.com>
2018-05-10 19:47:20 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
 def position_taken?(board, index)
   if board[index] == "X" || board[index] == "O"
     return true
-  elsif board[index] == " " || board[nil]
+  elsif board[index] == " "
     return false
   end
 end
